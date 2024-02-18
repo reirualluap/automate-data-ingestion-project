7fb83c4a73fa8abff8f050409f229b16c1d0dc07
Paul LAURIER <paullaurier@yahoo.fr>
2024-02-18 09:41:30 +0100

    V1 Extract : * Creating a dv3f class with extract method 	* Takes annee, scope as mandatory arguments and coddep, codreg as variable args 	* Variable api end point 	* Raise error for code 400-500 or empty results * Using logging module to track usage and debug * Dumping json in __repr__ to reuse data

--- extract.py
+++ extract.py
@@ -1,47 +1,80 @@
+
+# with open('results.json', 'w') as f:
+#     json.dump(data["results"], f, indent=4)
+        
+
+
+### 
+
+import logging
 import requests
 import json
 
-region_code = 44
-annee = 2022
-ordering = None
-page = None
-page_size = None
+# import hydra
+# https://hydra.cc/docs/intro/
 
-# URL de l'API
-url = f"https://apidf-preprod.cerema.fr/indicateurs/dv3f/regions/annuel/{region_code}/"
+# import dlt
+# https://dlthub.com/docs/intro
+# https://dlthub.com/docs/dlt-ecosystem/destinations/duckdb
 
-# Paramètres de la requête (si nécessaire)
-params = {
-    'annee': str(annee),
-    'ordering': str(ordering),
-    'page':page,
-    'page_size':page
-}
+# import duckdb
+# https://duckdb.org/docs/installation/?version=latest&environment=python
 
-params = {key: value for key, value in params.items() if value}
+class dv3f():
+    def __init__(self):
+        logging.basicConfig(level=logging.INFO, filename='dv3f.log', filemode='a', format='%(asctime)s - %(levelname)s - %(message)s')
+        self.logger = logging.getLogger(__name__)
 
-# Envoi de la requête GET à l'API
-response = requests.get(url, params=params)
-
-# Vérification du code de statut HTTP
-if response.status_code == 200:
-    nb_results = len(response.json()['results'])
-    if nb_results == 0:
-        raise BaseException("La requête a abouti mais le contenu est vide")
-    # Conversion de la réponse JSON en dictionnaire Python
-    else: 
-        data = response.json()
-        if nb_results == 1:
-            print(f"{nb_results} résultat a été trouvé")
-        else:
-            print(f"{nb_results} résultats ont été trouvés")
-    # Enregistrement des résultats dans un fichier
-        with open('results.json', 'w') as f:
-            json.dump(data["results"], f, indent=4)
+    def get_data(self, annee=None, scope=None,coddep=None, codreg=None, **kwargs):
         
-        print("Les résultats ont été enregistrés dans le fichier 'results.json'.")
-else:
-    print("La requête a échoué avec le code de statut:", response.status_code)
+        self.kwargs = kwargs
+        self.scope = scope
+        self.annee = annee
+        self.coddep = coddep
+        self.codreg = codreg
+        self.api_endpoint = None
+ 
+        if self.scope in ["region","reg"]:
+            self.api_endpoint = f"https://apidf-preprod.cerema.fr/indicateurs/dv3f/regions/annuel/{self.codreg}/"
+        elif self.scope in ["departement","dep"]:
+            self.api_endpoint = f"https://apidf-preprod.cerema.fr/indicateurs/dv3f/departements/annuel/{self.coddep}/"
+        else:
+            raise ValueError("Invalid scope value. Valid values are 'region' or 'departement'.")
+        self.logger.info(f"Annee : {self.annee}, Scope: {self.scope}, Kwargs: {kwargs}")
+
+        self.params = {
+            'annee': self.annee,
+            'ordering': kwargs.get("ordering"),
+            'page': kwargs.get("page"),
+            'page_size': kwargs.get("page_size")
+        }
+
+        self.params = {key: value for key, value in self.params.items() if value}
+        response = requests.get(self.api_endpoint, params=self.params)
+        self.logger.info(f"api_endpoint : {response.url} , Response : {response.status_code}, Response_nb : {response.json()['count']}")
+
+        if response.status_code == 200:
+            nb_results = len(response.json()['results'])
+            # self.logger.info(f"nb_results : {nb_results}")
+            if nb_results == 0:
+                raise BaseException("La requête a abouti mais le contenu est vide")
+            
+            else: 
+                data = response.json()
+                if nb_results == 1:
+                    print(f"{nb_results} résultat a été trouvé")
+                else:
+                    print(f"{nb_results} résultats ont été trouvés")
+
+                self.data = data["results"]
+
+        else:
+            print("La requête a échoué avec le code de statut:", response.status_code)
+        # return data["results"]
+        ### voir comment retourner les résultats dans l'object
+    def __repr__(self):
+        return json.dumps(self.data)
 
 
 
+# Switch coddep&codreg into code or raise error if scope=dep and correg
---
+++ scripts/extract.py
@@ -0,0 +1,84 @@
+
+# with open('results.json', 'w') as f:
+#     json.dump(data["results"], f, indent=4)
+        
+
+
+### 
+
+import logging
+import requests
+import json
+
+# import hydra
+# https://hydra.cc/docs/intro/
+
+# import dlt
+# https://dlthub.com/docs/intro
+# https://dlthub.com/docs/dlt-ecosystem/destinations/duckdb
+
+# import duckdb
+# https://duckdb.org/docs/installation/?version=latest&environment=python
+
+class dv3f():
+    def __init__(self):
+        logging.basicConfig(level=logging.INFO, filename='dv3f.log', filemode='a', format='%(asctime)s - %(levelname)s - %(message)s')
+        self.logger = logging.getLogger(__name__)
+
+    def get_data(self, annee=None, scope=None,coddep=None, codreg=None, **kwargs):
+        
+        self.kwargs = kwargs
+        self.scope = scope
+        self.annee = annee
+        self.coddep = coddep
+        self.codreg = codreg
+        self.api_endpoint = None
+ 
+        if self.scope in ["region","reg"]:
+            self.api_endpoint = f"https://apidf-preprod.cerema.fr/indicateurs/dv3f/regions/annuel/{self.codreg}/"
+        elif self.scope in ["departement","dep"]:
+            self.api_endpoint = f"https://apidf-preprod.cerema.fr/indicateurs/dv3f/departements/annuel/{self.coddep}/"
+        else:
+            raise ValueError("Invalid scope value. Valid values are 'region' or 'departement'.")
+        self.logger.info(f"Annee : {self.annee}, Scope: {self.scope}, Kwargs: {kwargs}")
+
+        self.params = {
+            'annee': self.annee,
+            'ordering': kwargs.get("ordering"),
+            'page': kwargs.get("page"),
+            'page_size': kwargs.get("page_size")
+        }
+
+        self.params = {key: value for key, value in self.params.items() if value}
+        response = requests.get(self.api_endpoint, params=self.params)
+        self.logger.info(f"api_endpoint : {response.url} , Response : {response.status_code}, Response_nb : {response.json()['count']}")
+
+        if response.status_code == 200:
+            nb_results = len(response.json()['results'])
+            # self.logger.info(f"nb_results : {nb_results}")
+            if nb_results == 0:
+                raise BaseException("La requête a abouti mais le contenu est vide")
+            
+            else: 
+                data = response.json()
+                if nb_results == 1:
+                    print(f"{nb_results} résultat a été trouvé")
+                else:
+                    print(f"{nb_results} résultats ont été trouvés")
+
+                self.data = data["results"]
+
+        else:
+            print("La requête a échoué avec le code de statut:", response.status_code)
+        # return data["results"]
+        ### voir comment retourner les résultats dans l'object
+    def __repr__(self):
+        return json.dumps(self.data)
+
+
+new_dv = dv3f()
+new_dv.get_data(scope="dep", coddep=59)
+
+print(new_dv)
+
+# Switch coddep&codreg into code or raise error if scope=dep and correg
