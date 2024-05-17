dc19646e5d6cc6be4e05105cb2448c268afa1caf
Paul LAURIER <paullaurier@yahoo.fr>
2024-05-17 18:04:54 +0200

    Fixing local exec issues

--- scripts/extract_load.py
+++ scripts/extract_load.py
@@ -11,19 +11,20 @@ with initialize(version_base=None, config_path="config", job_name="pipeline"):
 
 logger.add(f"{cfg.log.path}/{cfg.log.name}")
 
-class dv3f():
+
+class dv3f:
     def __init__(self):
         """
         Initializes the object and sets up logging configuration.
-        
+
         This method configures the logging system with basic settings, including log level,
         output file, and format. It initializes a logger object for use within the class.
         """
-    
-    def get_data(self, annee=None, scope=None,code=None, **kwargs):
+
+    def get_data(self, annee=None, scope=None, code=None, **kwargs):
         """
         Retrieves data from an API endpoint based on specified parameters.
-        
+
         Args:
             annee (int, optional): The year for which data is to be retrieved.
             scope (str, optional): The scope of the data, either 'region' or 'departement'.
@@ -47,76 +48,74 @@ class dv3f():
         self.annee = annee
         self.code = code
         self.api_endpoint = None
+        data = None
 
         ## Returns a tuple, translate into a mapping key
-        scope_code=kwargs.get("scope_code")
-        # scope_code=f"{scope_code[0]}_{scope_code[1]}"
+        scope_code = kwargs.get("scope_code")
 
         logger.info(f"Starting get task for : {scope_code}")
 
-        if self.scope in ["region","reg"]:
+        if self.scope in ["region", "reg"]:
             self.api_endpoint = f"https://apidf-preprod.cerema.fr/indicateurs/dv3f/regions/annuel/{self.code}/"
-        elif self.scope in ["departement","dep"]:
+        elif self.scope in ["departement", "dep"]:
             self.api_endpoint = f"https://apidf-preprod.cerema.fr/indicateurs/dv3f/departements/annuel/{self.code}/"
         else:
-            raise ValueError("Invalid scope value. Valid values are 'region' or 'departement'.")
-        
+            raise ValueError(
+                "Invalid scope value. Valid values are 'region' or 'departement'."
+            )
+
         logger.debug(f"Annee : {self.annee}, Scope: {self.scope}, Kwargs: {kwargs}")
 
         self.params = {
-            'annee': self.annee,
-            'ordering': kwargs.get("ordering"),
-            'page': kwargs.get("page"),
-            'page_size': kwargs.get("page_size")
+            "annee": self.annee,
+            "ordering": kwargs.get("ordering"),
+            "page": kwargs.get("page"),
+            "page_size": kwargs.get("page_size"),
         }
 
         self.params = {key: value for key, value in self.params.items() if value}
-         
+
         response = requests.get(self.api_endpoint, params=self.params)
-        
+
         if response.status_code == 200:
-            nb_results = len(response.json()['results'])
-            logger.debug(f"api_endpoint : {response.url}, Response_nb : {response.json()['count']}")
+            nb_results = len(response.json()["results"])
+            logger.debug(
+                f"api_endpoint : {response.url}, Response_nb : {response.json()['count']}"
+            )
             if nb_results == 0:
                 e = "La requête a abouti mais le contenu est vide"
                 logger.error(e)
-                raise BaseException(e)
-            else: 
-                data = response.json()
-                if nb_results == 1:
-                    print(f"{nb_results} résultat a été trouvé")
-                else:
-                    print(f"{nb_results} résultats ont été trouvés")
 
-                self.data = pd.json_normalize(data["results"])
+            else:
+                data = response.json()
+                data = pd.json_normalize(data["results"])
         else:
-            e = (f"La requête a échoué avec le code de statut: {response.status_code}")
+            e = f"La requête a échoué avec le code de statut: {response.status_code}"
             logger.error(e)
-            raise BaseException(e)
 
         logger.success(f"Get task ended for : {scope_code}")
-        
-        if 'data' in dir(self):
-            return self.data, scope_code
-        # elif data is not None:
-        #     return scope_code
+
+        if data is not None:
+            return data, scope_code
         else:
             raise ValueError("No data provided")
 
-
     def __repr__(self):
-        if 'data' in dir(self):
-           return self.data
+        if "data" in dir(self):
+            return self.data
         else:
-            raise ValueError("The object is empty. Cannot process empty data, please use get_data() method first.")
-            
+            raise ValueError(
+                "The object is empty. Cannot process empty data, please use get_data() method first."
+            )
+
     def __str__(self):
-        if 'data' in dir(self):
-           return str(self.data.head(5))
+        if "data" in dir(self):
+            return str(self.data.head(5))
         else:
-            raise ValueError("The object is empty. Cannot process empty data, please use get_data() method first.")
-    
-    ## Check input
+            raise ValueError(
+                "The object is empty. Cannot process empty data, please use get_data() method first."
+            )
+
     def transform_data(self, data, scope_code):
         """
         Transforms the provided DataFrame using a series of operations including melting, pivoting, and adding a unique identifier (UID).
@@ -128,74 +127,79 @@ class dv3f():
             None
 
         This method logs the start of the transformation task, performs the transformation operations,
-        and logs the completion of the task. 
+        and logs the completion of the task.
         """
 
         scope, code = scope_code
-        self.data = data
 
         logger.info(f"Starting transform task for : {scope_code}")
-        
-        m = hashlib.sha256()
-        if 'data' in dir(self):
-            df = pd.DataFrame(self.data)
-        elif data is not None:
+
+        if data is not None:
             df = pd.DataFrame(data)
         else:
             raise ValueError("No data provided")
 
         # Liste des colonnes à garder inchangées
-        if scope in ["region","reg"]:
-            id_vars = ['annee', 'reg', 'libreg']
-        elif scope in ["departement","dep"]:
-            id_vars = ['annee', 'dep', 'libdep']
+        if scope in ["region", "reg"]:
+            id_vars = ["annee", "reg", "libreg"]
+        elif scope in ["departement", "dep"]:
+            id_vars = ["annee", "dep", "libdep"]
         else:
-            raise ValueError("Invalid scope value. Valid values are 'region' or 'departement'.")
+            raise ValueError(
+                "Invalid scope value. Valid values are 'region' or 'departement'."
+            )
 
         # Réorganiser les données en utilisant melt et extraire le suffixe
-        df_melted = df.melt(id_vars=id_vars, var_name='cod_full', value_name='valeur')
+        df_melted = df.melt(id_vars=id_vars, var_name="cod_full", value_name="valeur")
 
         # Extraire le suffixe de la colonne cod_full
-        df_melted[['cod_full','cod']] = df_melted['cod_full'].str.rsplit('_', n=1, expand=True)
-        df_melted['cod'] = df_melted.apply(lambda row: row['cod'].replace('cod',''), axis=1)
+        df_melted[["cod_full", "cod"]] = df_melted["cod_full"].str.rsplit(
+            "_", n=1, expand=True
+        )
+        df_melted["cod"] = df_melted.apply(
+            lambda row: row["cod"].replace("cod", ""), axis=1
+        )
 
         # Pivoter la colonne cod_full
-        df_pivoted = df_melted.pivot_table(index=id_vars + ['cod'], columns='cod_full', values='valeur', aggfunc='first').reset_index()
+        df_pivoted = df_melted.pivot_table(
+            index=id_vars + ["cod"],
+            columns="cod_full",
+            values="valeur",
+            aggfunc="first",
+        ).reset_index()
 
         # Ajout d'une clé de déduplication (UID)
-        if scope in ["region","reg"]:
-            df_pivoted['uid'] = df_pivoted.apply(
-            lambda row: hashlib.sha256(
-                str(row['annee']).encode('utf-8') + 
-                str(row['reg']).encode('utf-8') + 
-                str(row['cod']).encode('utf-8')
-            ).hexdigest(), 
-            axis=1
-        )
-        elif scope in ["departement","dep"]:
-            df_pivoted['uid'] = df_pivoted.apply(
-            lambda row: hashlib.sha256(
-                str(row['annee']).encode('utf-8') + 
-                str(row['dep']).encode('utf-8') + 
-                str(row['cod']).encode('utf-8')
-            ).hexdigest(), 
-            axis=1
-        )
+        if scope in ["region", "reg"]:
+            df_pivoted["uid"] = df_pivoted.apply(
+                lambda row: hashlib.sha256(
+                    str(row["annee"]).encode("utf-8")
+                    + str(row["reg"]).encode("utf-8")
+                    + str(row["cod"]).encode("utf-8")
+                ).hexdigest(),
+                axis=1,
+            )
+        elif scope in ["departement", "dep"]:
+            df_pivoted["uid"] = df_pivoted.apply(
+                lambda row: hashlib.sha256(
+                    str(row["annee"]).encode("utf-8")
+                    + str(row["dep"]).encode("utf-8")
+                    + str(row["cod"]).encode("utf-8")
+                ).hexdigest(),
+                axis=1,
+            )
         else:
-            raise ValueError("Invalid scope value. Valid values are 'region' or 'departement'.")
+            raise ValueError(
+                "Invalid scope value. Valid values are 'region' or 'departement'."
+            )
 
         # Renommer les colonnes si nécessaire
         df_pivoted.columns.name = None
 
         data = df_pivoted
+        # logger.debug(data.columns)
         logger.success(f"Transform task ended for : {scope_code}")
         return data, scope_code
 
-    def test(self):
-        # print(f"{cfg.schema.columns.keys()}")
-        # print(f"""CREATE TABLE IF NOT EXISTS {cfg.db.schema_name}.{cfg.db.tables.staging} ({', '.join([f"{key} {value.type}" for key, value in cfg.schema.columns.items()])}, PRIMARY KEY ({', '.join(cfg.schema.primary_keys)}));""")
-        pass
- 
     def load_data(self, data, scope_code):
         logger.info(f"Starting load task for : {scope_code}")
         scope, code = scope_code
@@ -206,11 +210,8 @@ class dv3f():
                 table_pk = table.schema.primary_keys
                 table_columns = table.schema.columns
 
-        with duckdb.connect(f"data/{cfg.db.db_name}.db") as con:
-
-            if 'data' in dir(self):
-                insertion_table = self.data
-            elif data is not None:
+        with duckdb.connect(f"data/{cfg.db.db_name}.duckdb") as con:
+            if data is not None:
                 insertion_table = data
             else:
                 raise ValueError("No data provided")
@@ -219,19 +220,21 @@ class dv3f():
             con.sql(f"CREATE SCHEMA IF NOT EXISTS {cfg.db.schema_name}")
 
             try:
-                con_obj = con.table(f"{cfg.db.schema_name}.{table_name}")
+                con.table(f"{cfg.db.schema_name}.{table_name}")
                 logger.info(f"Table {cfg.db.schema_name}.{table_name} exists")
             except Exception as e:
                 logger.warning(e)
                 logger.info(f"Creating new table as {cfg.db.schema_name}.{table_name}")
-                con.sql(f"""CREATE TABLE IF NOT EXISTS {cfg.db.schema_name}.{table_name} ({', '.join([f"{key} {value.type}" for key, value in table_columns.items()])}, PRIMARY KEY ({', '.join(table_pk)}));""")
+                con.sql(
+                    f"""CREATE TABLE IF NOT EXISTS {cfg.db.schema_name}.{table_name} ({', '.join([f"{key} {value.type}" for key, value in table_columns.items()])}, PRIMARY KEY ({', '.join(table_pk)}));"""
+                )
                 logger.warning(f"{cfg.db.schema_name}.{table_name} created")
-                                
+
             try:
                 logger.debug(f"Inserting into {cfg.db.schema_name}.{table_name}")
-                con.sql(f"INSERT OR REPLACE INTO {cfg.db.schema_name}.{table_name} BY NAME SELECT * FROM insertion_table;")
-                logger.success("Load task ended for : {scope_code}")
+                con.sql(
+                    f"INSERT OR REPLACE INTO {cfg.db.schema_name}.{table_name} BY NAME SELECT * FROM insertion_table;"
+                )
+                logger.success(f"Load task ended for : {scope_code}")
             except Exception as e:
                 logger.error(e)
-
-## RUFF CHECK PASSED
